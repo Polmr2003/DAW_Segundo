123471e2fde75796e0755c7ab8b4f8c68d44eba8
Polmr <pol.mr.2003@gmail.com>
2023-09-21 12:06:45 +0200

    vueling con menu, document.getElementByid

--- M06-Entorno Cliente/Ejemplo 1/js/index.js
+++ M06-Entorno Cliente/Ejemplo 1/js/index.js
@@ -61,9 +61,43 @@ let salutacions = function () {
 alert(salutacions());
 
 //funciones fletxa
-let salutacion = ()=>{
+let salutacion = () => {
     return "Hola array function";
 }
 
 //document
-document.getElementById("hello").innerHTML="hola a tots"; //cojemos el id que tenga de nombre hello en el hmtl i lo machacamos el contenido que tenga esta id con hola a tots
+document.getElementById("hello").innerHTML = "hola a tots"; //cojemos el id que tenga de nombre hello en el hmtl i lo machacamos el contenido que tenga esta id con hola a tots
+
+let valors = document.getElementById("h2");//ahora lo hacemos pero con una variable
+valors.innerHTML = "h2";
+
+let valors_array = document.getElementsByTagName("h2");//es un array
+console.log(valors_array([1].innerHTML))
+
+let classe = document.getElementsByClassName("intro");
+console.log(classe);
+
+console.log(document.querySelector("#hello").innerHTML); //accedo al id
+console.log(document.querySelector("h2.intro").innerHTML); //accedo la classe
+console.log(document.querySelectorAll(".intro")); //accedo a todos los tags con la classe intro
+
+
+//acceso a los atributos
+console.log(document.getElementById("link").href);
+
+//cambio de atributo
+document.getElementById("link").href = "http://proven.cat";
+
+//manera alternativa de incrustar una pagina
+document.write("hello hello"); // no respeta lo que hubiera antes, te lo mete a saco donde quepa
+
+//recojo un valor de un formulario, le pongo una condicion para hacerlo que es que clicke
+document.getElementById("clic").addEventListener("click", function () {
+
+    let my_name = document.getElementById("nom").value;
+
+    if (my_name == "David") {
+        alert("HOLA DAVID!!");
+
+    };
+});
