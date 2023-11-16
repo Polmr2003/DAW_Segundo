6689ac021e1007e94a133c0612a9de1695e4aeee
Pol Moreno <pol.mr.2003@gmail.com>
2023-11-16 13:47:42 +0100

    m06 - api

--- M06-Entorno Cliente/Api/api-rest/index.js
+++ M06-Entorno Cliente/Api/api-rest/index.js
@@ -38,9 +38,58 @@ var connection = mysql.createConnection({
     user: 'root',
     password: ''
 });
+
+
 /////// fem servir la BBDD que tenim
+
+// Ruta GET para autenticación
+// establecemos una ruta GET en la aplicación Express. La ruta es '/vueling/login', lo que significa que manejará solicitudes GET enviadas a esa URL específica.
+app.get('/vueling/login', function (req, res) {
+    // extraemos las propiedades usuari, password. desestructuramos el Objeto para extraer los valores de usuari y password de la consulta
+    const { usuari, password } = req.query;
+
+    // Seleccionamos todos los registros de la tabla 'users' donde el valor de 'usuari' coincide con el valor proporcionado y donde el valor de 'password' coincide con el valor proporcionado
+    connection.query(
+        'SELECT * FROM users WHERE usuari = ? AND password = ?',
+        [usuari, password],
+        function (error, results) {
+            // Manejo de errores
+            if (error) {
+                console.error('Error al verificar las credenciales:', error);
+
+                 // Envía una respuesta con un código de estado 500 (Bad Request) y un objeto JSON indicando el error
+                res.status(500).send({
+                    error: true,
+                    resultats: null,
+                    mensaje: "Error al verificar las credenciales"
+                });
+            } else {
+                if (results.length > 0) {
+
+                    // Envía una respuesta con un código de estado 200 (OK) y un objeto JSON indicando el éxito
+                    res.status(200).send({
+                        error: false,
+                        resultats: results,
+                        mensaje: "Inicio de sesión exitoso"
+                    });
+                } else {
+
+                    // Envía una respuesta con un código de estado 400 (no results found in the query) y un objeto JSON indicando el error
+                    res.status(401).send({
+                        error: true,
+                        resultats: null,
+                        mensaje: "Credenciales inválidas"
+                    });
+                }
+            }
+        }
+    );
+});
+
+// Ruta POST para la inserción de usuarios
+// establecemos una ruta POST en la aplicación Express. La ruta es '/vueling/login', lo que significa que manejará solicitudes POST enviadas a esa URL específica.
 app.post('/vueling/login', function (req, res) {
-    // Desestructura los datos del cuerpo de la solicitud (request)
+    // extraemos las propiedades nom, cognom, gmail, password, y usuari del cuerpo de la solicitud (req.body). Estos valores se envían desde el cliente como parte de la solicitud POST
     const { nom, cognom, gmail, password, usuari } = req.body;
 
     // Realiza una consulta SQL para insertar un nuevo usuario en la tabla 'users'
