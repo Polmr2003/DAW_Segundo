7d7c6292757bd404fb8df1c830122c3a53978f08
Pol Moreno <pol.mr.2003@gmail.com>
2023-11-22 10:21:21 +0100

    m06 - Jquery

--- M06-Entorno Cliente/01-Ejercicios/JQuery/js/index.js
+++ M06-Entorno Cliente/01-Ejercicios/JQuery/js/index.js
@@ -16,26 +16,32 @@ $(document).ready(function () {
             let edad = $("#edad").val();
 
             //-------------------------- Realizar validaciones aquí ---------------------------------
-            
+
             // Verifico nombre
-            if (!isNaN(texto_input)) {
+            if (texto_input.length < 1) {
+                $("#Err_Nombre").html("No puede estar vacio");
+            } else if (!isNaN(texto_input)) {
                 $("#Err_Nombre").html("Solo puedes poner Letras");
-            }else{
+            } else {
                 $("#Err_Nombre").html("");
             }
 
             // Verifico dni
-            if (validarDNI(DNI)) {
+            if (DNI.length < 1) {
+                $("#Err_Dni").html("No puede estar vacio");
+            } else if (!validarDNI(DNI)) {
                 $("#Err_Dni").html("DNI invalido");
-            }else{
+            } else {
                 $("#Err_Dni").html("");
             }
 
             //Verfico edad
-            if (isNaN(texto_input)) {
-                $("#Err_Nombre").html("Solo puedes poner Letras");
-            }else{
-                $("#Err_Nombre").html("");
+            if (edad.length < 1) {
+                $("#Err_Edad").html("No puede estar vacio");
+            } else if (isNaN(edad)) {
+                $("#Err_Edad").html("Solo puedes poner Numeros");
+            } else {
+                $("#Err_Edad").html("");
             }
 
         },
@@ -44,18 +50,27 @@ $(document).ready(function () {
 });
 
 function validarDNI(dni) {
-    // Expresión regular para comprobar que el DNI tiene el formato correcto
+    // Expresión regular para comprobar que el DNI tiene el formato correcto, que tenga exactamente 8 dígitos seguidos por una letra 
     var dniRegex = /^\d{8}[a-zA-Z]$/;
 
+    // Si el formato no es válido
     if (!dniRegex.test(dni)) {
-        return false; // El formato no es válido
+        return false; 
     }
 
+    // Esta cadena contiene las letras de control correspondientes a los números del DNI.
     var letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+    // Extrae los primeros 8 dígitos del DNI
     var numero = dni.substr(0, 8);
+
+    // Extrae la letra del DNI y la convierte a mayúscula
     var letraUsuario = dni.substr(8, 1).toUpperCase();
+
+    // Calculamos la letra del DNI como si fuera un DNI real de España
     var letraCalculada = letras[numero % 23];
 
+    // Miramos si la letra que a puesto el usuario es la que hemos calculado para saber si el dni es real
     return letraUsuario === letraCalculada;
 }
 
