bb786e7bfc532ea8a9616c8b5a3a21f0f984e8a4
Polm04 <pol.mr.2003@gmail.com>
2023-11-24 18:59:36 +0100

    m06

--- M06-Entorno Cliente/01-Ejercicios/01-Ejercicios_JQuery/JQuery_Llistat_de_ciutats/js/index.js
+++ M06-Entorno Cliente/01-Ejercicios/01-Ejercicios_JQuery/JQuery_Llistat_de_ciutats/js/index.js
@@ -4,37 +4,39 @@ $(document).ready(function () {
     let ciutats = ["Barcelona", "Girona", "Tarragona", "Lleida", "Valencia"];
 
     //selector en el HTML donde vamos a poner las ciudades
-    let ciudades_origen = $("#ciudades");
+    let ciudades = $("#ciudades");
 
     //añadir las ciudades al selector en el HTML
-    for (let i = 0; i < ciutats.length; i++) {
-        ciudades_origen.append($("<li>").text(ciutats[i]));
-    }
+    let posicion_ciudad = 0;
 
-    // Añadir un elemento al final de la lista
+    // Añadir un elemento al final de la lista, con append
     $("#end_list").click(function () {
-        let nuevaCiudad = prompt("Introduce el nombre de la ciudad:");
-        if (nuevaCiudad) {
-            ciudades_origen.append($("<li>").text(nuevaCiudad));
+        // verficamos si aun hay ciudades en el array
+        if (posicion_ciudad < ciutats.length) {
+            // si aun no se an añadido todas las ciudades
+            ciudades.append($("<li>").text(ciutats[posicion_ciudad]));
+            posicion_ciudad++;
         }
     });
 
-    // Añadir un elemento al comienzo de la lista
+    // Añadir un elemento al comienzo de la lista, con prepend
     $("#start_list").click(function () {
-        let nuevaCiudad = prompt("Introduce el nombre de la ciudad:");
-        if (nuevaCiudad) {
-            ciudades_origen.prepend($("<li>").text(nuevaCiudad));
+        // verficamos si aun hay ciudades en el array
+        if (posicion_ciudad < ciutats.length) {
+            // si aun no se an añadido todas las ciudades
+            ciudades.prepend($("<li>").text(ciutats[posicion_ciudad]));
+            posicion_ciudad++;
         }
     });
 
     // Eliminar el último elemento de la lista
     $("#del_end").click(function () {
-        ciudades_origen.children("li:last").remove();
+        ciudades.children("li:last").remove();
     });
 
     // Eliminar el primer y segundo elemento de la lista
     $("#del_first_and_second").click(function () {
-        ciudades_origen.children("li:first").remove();
-        ciudades_origen.children("li:first").remove();
+        ciudades.children("li:first").remove();
+        ciudades.children("li:first").remove();
     });
 });
