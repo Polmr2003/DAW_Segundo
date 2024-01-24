75b2028da2180147e1dd1f28909fb5be0fc8380b
Pol Moreno <pol.mr.2003@gmail.com>
2024-01-24 13:04:12 +0100

    Vendors

--- M07-Entorno Servidor/EJERCICIOS/MVC/Examen_uf2/view/options/Books/Books.php
+++ M07-Entorno Servidor/EJERCICIOS/MVC/Examen_uf2/view/options/Books/Books.php
@@ -1,4 +1,14 @@
+<head>
+    <title>Examen</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Enlaces a Bootstrap (localmente) -->
+    <link rel="stylesheet" href="../../../Vendors/bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script type="text/javascript" src="../../../Vendors/bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+
+</head>
 <div id="content">
+    <h1>Books</h1>
     <?php
     require_once "../../../util/Funciones_importantes/funcions_archivos.php";
     require_once "../../../util/Funciones_importantes/funcions_array.php";
