efcb670c8cffbedda26c68c1d94d40b1668c04b9
Pol Moreno <pol.mr.2003@gmail.com>
2024-01-31 13:51:17 +0100

    m07 - larabel

--- M07-Entorno Servidor/01-Teoria/Larabel/test0.0/routes/web.php
+++ M07-Entorno Servidor/01-Teoria/Larabel/test0.0/routes/web.php
@@ -13,6 +13,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// cuando el usuario no pone nada ("/") nos redirigira a cursos ("/cursos") || para que esto funcione hay que comentar la linea get que tenga la /
+Route::redirect('/', 'cursos');
+
+// cuando el usuario no pone nada ("/") nos redirigira aqui
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Bienvenido a la paguina web";
 });
+
+// cuando el usuario pone cursos ("/cursos") nos redirigira aqui
+Route::get('cursos', function () {
+    return "Bienvenido a cursos";
+});
+
+// cuando el usuario pone cursos ("/cursos/create") nos redirigira aqui
+Route::get('cursos/create', function () {
+    return "Bienvenido a cursos create";
+});
+
+// cuando el usuario pone cursos ("/cursos/{curso}/{categoria}") nos redirigira aqui || si queremos que sea opcional algo le tenemos que poner "?"
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    // si la variable de categoria esta en null pondremos solo el curso
+    if ($categoria != null) {
+        return "Bienvenido al curso: $curso, de la categoria: $categoria";
+    } else {
+        return "Bienvenido al curso: $curso";
+    }
+});
+
+/*
+|--------------------------------------------------------------------------
+| Protect routes with regular expresions
+|--------------------------------------------------------------------------
+*/
+
+Route::get('cursos/{curso}', function ($curso) {
+    return "Bienvenido al curso: $curso";
+})->where('curso', '[A-Za-z]+'); // para poner dos restricciones: ->where(['curso' => '[0-9]+', 'categoria' => '[a-z]+'])
