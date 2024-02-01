c352c9aec16bb9f734189e0a35dee697ced30d8a
Pol Moreno <pol.mr.2003@gmail.com>
2024-02-01 14:36:21 +0100

    m07 - larabel

--- M07-Entorno Servidor/01-Teoria/05-Larabel/test0.0/resources/views/welcome.blade.php
+++ M07-Entorno Servidor/01-Teoria/05-Larabel/test0.0/resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -137,4 +137,36 @@
             </div>
         </div>
     </body>
+</html> --}}
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+</head>
+
+<body>
+    <a href="{{ route('posts.index') }}">List</a>
+    <ul>
+        <li>
+            <a href="{{ route('posts.show', 1) }}">1</a>
+        </li>
+        <li>
+            <a href="{{ route('posts.show', 2) }}">2</a>
+        </li>
+        <li>
+            <a href="{{ route('posts.show', 3) }}">3</a>
+        </li>
+    </ul>
+
+</body>
+
 </html>
--- M07-Entorno Servidor/01-Teoria/05-Larabel/test0.0/routes/web.php
+++ M07-Entorno Servidor/01-Teoria/05-Larabel/test0.0/routes/web.php
@@ -14,44 +14,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// cuando el usuario no pone nada ("/") nos redirigira a cursos ("/cursos") || para que esto funcione hay que comentar la linea get que tenga la /
-Route::redirect('/', 'cursos');
+// // cuando el usuario no pone nada ("/") nos redirigira a cursos ("/cursos") || para que esto funcione hay que comentar la linea get que tenga la /
+// Route::redirect('/', 'posts');
 
-// cuando el usuario no pone nada ("/") nos redirigira aqui
+// // cuando el usuario no pone nada ("/") nos redirigira aqui
 Route::get('/', function () {
-    // return view('welcome');
-    return "Bienvenido a la paguina web";
+    return view('welcome');
+    // return "Bienvenido a la paguina web";
 });
 
-// cuando el usuario pone cursos ("/cursos") nos redirigira aqui
-Route::get('cursos', function () {
-    return "Bienvenido a cursos";
-});
+// // cuando el usuario pone cursos ("/cursos") nos redirigira aqui
+// Route::get('cursos', function () {
+//     return "Bienvenido a cursos";
+// });
 
-// cuando el usuario pone cursos ("/cursos/create") nos redirigira aqui
-Route::get('cursos/create', function () {
-    return "Bienvenido a cursos create";
-});
+// // cuando el usuario pone cursos ("/cursos/create") nos redirigira aqui
+// Route::get('cursos/create', function () {
+//     return "Bienvenido a cursos create";
+// });
 
-// cuando el usuario pone cursos ("/cursos/{curso}/{categoria}") nos redirigira aqui || si queremos que sea opcional algo le tenemos que poner "?"
-Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
-    // si la variable de categoria esta en null pondremos solo el curso
-    if ($categoria != null) {
-        return "Bienvenido al curso: $curso, de la categoria: $categoria";
-    } else {
-        return "Bienvenido al curso: $curso";
-    }
-});
+// // cuando el usuario pone cursos ("/cursos/{curso}/{categoria}") nos redirigira aqui || si queremos que sea opcional algo le tenemos que poner "?"
+// Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+//     // si la variable de categoria esta en null pondremos solo el curso
+//     if ($categoria != null) {
+//         return "Bienvenido al curso: $curso, de la categoria: $categoria";
+//     } else {
+//         return "Bienvenido al curso: $curso";
+//     }
+// });
 
-/*
-|--------------------------------------------------------------------------
-| Protect routes with regular expresions
-|--------------------------------------------------------------------------
-*/
+// /*
+// |--------------------------------------------------------------------------
+// | Protect routes with regular expresions
+// |--------------------------------------------------------------------------
+// */
 
-Route::get('cursos/{curso}', function ($curso) {
-    return "Bienvenido al curso: $curso";
-})->where('curso', '[A-Za-z]+'); // para poner dos restricciones: ->where(['curso' => '[0-9]+', 'categoria' => '[a-z]+'])
+// Route::get('cursos/{curso}', function ($curso) {
+//     return "Bienvenido al curso: $curso";
+// })->where('curso', '[A-Za-z]+'); // para poner dos restricciones: ->where(['curso' => '[0-9]+', 'categoria' => '[a-z]+'])
 
 
 
@@ -61,10 +61,10 @@ Route::get('cursos/{curso}', function ($curso) {
 |--------------------------------------------------------------------------
 */
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
-Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.crate');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
 
 /*
 |--------------------------------------------------------------------------
@@ -72,7 +72,7 @@ Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 |--------------------------------------------------------------------------
 */
 
-Route::post('/post', [PostController::class, 'store']);
+Route::post('/post', [PostController::class, 'store'])->name('posts.store');
 
 /*
 |--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ Route::post('/post', [PostController::class, 'store']);
 |--------------------------------------------------------------------------
 */
 
-Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
 
 /*
 |--------------------------------------------------------------------------
@@ -88,5 +88,5 @@ Route::put('/posts/{post}', [PostController::class, 'update']);
 |--------------------------------------------------------------------------
 */
 
-Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
