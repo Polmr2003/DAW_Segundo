b6f79f0123cda928c97dec1387b37e2847ac5e26
Pol Moreno <pol.mr.2003@gmail.com>
2024-02-22 09:43:14 +0100

    m07

--- M07-Entorno Servidor/01-Teoria/05-Larabel/Mascotas_Clinic_Larabel/routes/web.php
+++ M07-Entorno Servidor/01-Teoria/05-Larabel/Mascotas_Clinic_Larabel/routes/web.php
@@ -16,10 +16,10 @@ use App\Http\Controllers\OwnerController;
 |
 */
 
-// cuando no ponga nada el usuario nos ira a la ruta que le halla puesto en este caso el controlador OwnerController 
+// cuando no ponga nada el usuario nos ira a la ruta que le halla puesto en este caso el controlador OwnerController
 // -> i nos ejecutara el metodo que le hemos puesto en este caso 'index'
 Route::get('/', [App\Http\Controllers\OwnerController::class, 'index']);
 
-// recurso owner con el crud, lo definimos con resource i el nombre del controlador, cuando lo creas ya te define 
+// recurso owner con el crud, lo definimos con resource i el nombre del controlador, cuando lo creas ya te define
 // -> las rutas con el nombre que le hallas puesto en este caso Owner i luego el metodo (Owner.index, ...)
 Route::resource('Owner', OwnerController::class);
