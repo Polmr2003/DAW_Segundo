728c1cbecf09daa464592859ab8c666b4095be10
Polm04 <pol.mr.2003@gmail.com>
2024-02-22 21:19:33 +0100

    m07 - larabel

--- M07-Entorno Servidor/01-Teoria/05-Larabel/Mascotas_Clinic_Larabel/resources/views/Layouts/app.blade.php
+++ M07-Entorno Servidor/01-Teoria/05-Larabel/Mascotas_Clinic_Larabel/resources/views/Layouts/app.blade.php
@@ -3,13 +3,17 @@
 <!-- Header de la paguina -->
 @include('Menu.header')
 <link href="{{ asset('css/body.css') }}" rel="stylesheet">
-<img src="{{ asset('img/vet.png') }}" alt="" class="img"><h1>Clínica Veterinaria</h1>
+<img src="{{ asset('img/vet.png') }}" alt="" class="img">
+<h1>Clínica Veterinaria</h1>
+
 <body>
     <!-- Incluimos la vista navigation donde esta el menu -->
     @include('Menu.navigation')
 
     <!-- Contenido de la pagina -->
     @yield('content')
+
+    <!-- <x-Layouts.app> -->
 
 </body>
 
