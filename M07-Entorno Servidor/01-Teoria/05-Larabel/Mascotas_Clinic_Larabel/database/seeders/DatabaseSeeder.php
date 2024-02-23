8f54fe63ab328b27b00357bddc51d3b5f092274b
Pol Moreno <pol.mr.2003@gmail.com>
2024-02-23 11:32:29 +0100

    m07 - mascotas larabel acabado

--- M07-Entorno Servidor/01-Teoria/05-Larabel/Mascotas_Clinic_Larabel/database/seeders/DatabaseSeeder.php
+++ M07-Entorno Servidor/01-Teoria/05-Larabel/Mascotas_Clinic_Larabel/database/seeders/DatabaseSeeder.php
@@ -14,9 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // añadimos los datos que hay en el factory
+        \App\Models\Owner::factory()->create();
     }
 }
