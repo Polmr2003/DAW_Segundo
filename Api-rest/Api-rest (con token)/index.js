ebe2f3da49b0ee960f7dd4b9d82386388c9fe1e2
Polm04 <pol.mr.2003@gmail.com>
2024-03-05 21:59:12 +0100

    Api con token

--- Api-rest/Api-rest (con token)/index.js
+++ Api-rest/Api-rest (con token)/index.js
@@ -6,21 +6,20 @@
 //  */
 
 //usuaris  fixos per a fer la prova. 
-//L'ideal seria amb BBDD
-const users = [
-    {
-        username: 'john',
-        password: 'password123admin',
-        role: 'admin'
-    }, {
-        username: 'anna',
-        password: 'password123member',
-        role: 'member'
-    }
-];
+// const users = [
+//     {
+//         username: 'john',
+//         password: 'password123admin',
+//         role: 'admin'
+//     }, {
+//         username: 'anna',
+//         password: 'password123member',
+//         role: 'member'
+//     }
+// ];
 
 //carrega del framework express
-const express =require('express');
+const express = require('express');
 const app = express();
 
 //carrega del paquet jsonwebtoken
@@ -35,26 +34,87 @@ app.use(bodyParser.json());
 //paraula secreta necessària en el token
 const accessTokenSecret = 'youraccesstokensecret';
 
+//creem una constant que farà de middleware a qui el faci servir (get i post de books)
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                console.log(err)
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
 //atenem la petició de login
 app.post('/login', (req, res) => {
     //res.sendFile(process.cwd()+"/view/index.html"); //angular dins del servidor
-    
-    // Read username and password from request body
-    const { username, password } = req.body;
+    // extraemos los valores del cuerpo de la solicitud (req.body). Estos valores se envían desde el cliente como parte de la solicitud POST
+    const { usuari, password } = req.body;
 
-    // Filter user from the users array by username and password
-    const user = users.find(u => { return u.username === username && u.password === password });
-//si trobem l'usuari a l'array
-    if (user) {
-        // generem un token
-        const accessToken = jwt.sign({ username: user.username,  role: user.role }, accessTokenSecret);
-        //enviem el token
-        res.json({
-            accessToken
+    // comprovamos lo que nos a llegado al servidor
+    // console.log("Usuario recibido en el servidor:", usuari);
+    // console.log("Contraseña recibida en el servidor:", password);
+
+    /// Realiza una consulta SQL para consultar la base de datos si existe el usuario i la contraseña
+    connection.query(
+        'SELECT * FROM users WHERE usuari = ? AND password = ?',
+        [usuari, password],
+        function (error, results) {
+            // Manejo de errores
+            if (error) {
+                // mostramos por consola el error
+                console.error('Error en la consulta de autenticación:', error.message);
+
+                // Si hay un error
+                // Enviamos una respuesta con un código de estado 500 ("Internal Server Error" (Error interno del servidor)) y un objeto JSON con:
+                // un booleno de error a true, un array de resultats a null i enviamos un mensage de error
+                res.status(500).json({
+                    error: true,
+                    results: null,
+                    message: "Error en la autenticación"
+                });
+            }
+
+            // mostramos el resultado (La informacion del usuario) si no a habido error en la consulta
+            console.log("Resultados de la consulta:", results);
+
+            // Verificar si se encontró una coincidencia
+            if (results.length === 1) {
+                // Si no a habido ningun error, a encontrado el usuario
+                // generem un token
+                const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret);
+
+                // Enviamos una respuesta con un código de estado 200 (OK) y un objeto JSON con:
+                // un booleno de error a false, un array de resultats con los resultados i enviamos un mensage satisfactorio
+                //enviem el token
+                res.status(200).json({
+                    error: false,
+                    results: results,
+                    token: accessToken,
+                    message: "Autenticación exitosa"
+                });
+            } else {
+                // Si no a encontrado ningin usuario
+                // Enviamos una respuesta con un código de estado 401 ( No a encontrado el usuario o "Unauthorized" (No autorizado)) y un objeto JSON con:
+                // un booleno de error a true, un array de resultats a null i enviamos un mensage de error
+                res.status(401).json({
+                    error: true,
+                    results: null,
+                    message: "Credenciales incorrectas"
+                });
+            }
         });
-    } else {
-        res.send('Username or password incorrect');
-    }
+
 });
 
 //creem un array de llibres
@@ -84,47 +144,27 @@ const books = [
         "year": 1315
     },
 ];
- 
-//creem una constant que farà de middleware a qui el faci servir (get i post de books)
-const authenticateJWT = (req, res, next) => {
-    const authHeader = req.headers.authorization;
 
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
-
-        jwt.verify(token, accessTokenSecret, (err, user) => {
-            if (err) {
-                console.log(err)
-                return res.sendStatus(403);
-            }
-
-            req.user = user;
-            next();
-        });
-    } else {
-        res.sendStatus(401);
-    }
-};
 
 //get de books: si passa el control del middleware hi entrarà 
 //i enviarà tots els llibres
-app.get('/books',authenticateJWT, (req, res) => {
+app.get('/books', authenticateJWT, (req, res) => {
     res.json(books);
 });
 
 //post de books: si passa el control del middleware hi entrarà 
 //i comprovarà el rol. Només si som admin ens deixa afegir un llibre
 
-app.post('/books',authenticateJWT, (req, res) => {
+app.post('/books', authenticateJWT, (req, res) => {
     //recollim el rol del user autenticat
-    const role =req.user.role;
+    const role = req.user.role;
 
-    if(role !== 'admin'){
+    if (role !== 'admin') {
         return res.sendStatus(403);
     }
-    const book=req.body
-   books.push(book);
-   res.send('Book added successfully');
+    const book = req.body
+    books.push(book);
+    res.send('Book added successfully');
 
 });
 
