d462bb2c8db541c88c31b926faff7c1f7c0799e7
Polm04 <pol.mr.2003@gmail.com>
2024-03-06 19:48:26 +0100

    m06 - uf4

--- M06-Entorno Cliente/02-Ejercicios/03-Ejercicios_Angular/my-app-guard+interceptor+JWT (src)/src/app/app-routing.module.ts
+++ M06-Entorno Cliente/02-Ejercicios/03-Ejercicios_Angular/my-app-guard+interceptor+JWT (src)/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { Com2Component } from './_com2/_com2.component';
 
 const rutes: Routes = [
 
-  { path: 'com1',  component: Com1Component },
+  { path: 'com1',  component: Com1Component },  //, canActivate:[MyGuardGuard]
   { path: 'com2', component: Com2Component },
   { path: 'home',  component: HomeComponentComponent },
   { path:'', redirectTo:'/com1', pathMatch:'full'}
